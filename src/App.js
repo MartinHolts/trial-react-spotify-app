cb2ce88b2b77721bf85abf68c5ad8438794ee253
MartinHolts <martinholtsmeier@gmail.com>
2024-06-23 16:15:08 +0300

    Added more spotify variables and redindented file.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Spotify React</h1>
-        <a href=''>Login to Spotify</a>
-      </header>
-    </div>
-  );
+	const CLIENT_ID = "4b68c0c6d7014e1ea92ea2109b3ddb93";
+	const REDIRECT_URI = "http://localhost:3000/";
+	const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+	const RESPONSE_TYPE = "token";
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Spotify React</h1>
+				<a href=''>Login to Spotify</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
